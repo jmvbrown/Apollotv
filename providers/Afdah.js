6ab15e2036819c784a28d6cfa02cfc8cebcca126
NBTX <nbtx@telegenic.me>
2018-09-14 22:43:42 +0100

    oops

--- providers/Afdah.js
+++ providers/Afdah.js
@@ -9,8 +9,7 @@ async function Afdah(queryString, sse) {
     const browser = await puppeteer.launch({args: ['--no-sandbox']});
     
     // These are all the same host I think. https://xmovies8.org isn't loading.
-    //const urls = ["https://afdah.org", "https://genvideos.com", "https://genvideos.co", "https://watch32hd.co", "https://putlockerhd.co"];
-    const urls = "https://afdah.org";
+    const urls = ["https://afdah.org", "https://genvideos.com", "https://genvideos.co", "https://watch32hd.co", "https://putlockerhd.co"];
     const promises = [];
 
     // Go to each url and scrape for links, then send the link to the client 
