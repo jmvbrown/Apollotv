acdc5c3856c6a1a2ea5b30900199a6e6f836f2d0
claws-dev <nxtvserverdev@protonmail.com>
2018-09-24 20:42:08 -0400

    My stuff got blown away. Gotta figure out my merges better...

---
+++ providers/movies/AZMovies.js
@@ -0,0 +1,77 @@
+const URL = require('url');
+const Promise = require('bluebird');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+
+async function Afdah(queryString, sse) {
+    // Start up the headless browser in no-sandbox mode to make it truly headless
+    const browser = await puppeteer.launch({args: ['--no-sandbox']});
+
+    // These are all the same host I think. https://xmovies8.org isn't loading.
+    const urls = ["https://afdah.org", "https://genvideos.com", "https://genvideos.co", "https://watch32hd.co", "https://putlockerhd.co", "https://xmovies8.org"];
+    const promises = [];
+
+    // Go to each url and scrape for links, then send the link to the client
+    async function scrape(url) {
+        try {
+            const usePlus = url === "https://putlockerhd.co" || url === "https://genvideos.co" || url === "https://watch32hd.co"
+            const html = await rp({
+                uri: `${url}/results?q=${usePlus ? queryString.replace(/ /, '%2B'): queryString}`,
+                timeout: 5000
+            });
+
+            let $ = cheerio.load(html);
+            let videoId = '';
+
+            $('.cell').toArray().some(element => {
+                const videoName = $(element).find('.video_title').text().trim();
+                if (videoName === queryString) {
+                    videoId = $(element).find('.video_title h3 a').attr('href').trim();
+                    return true;
+                }
+
+                return false;
+            });
+
+            const videoPageHtml = await rp({
+                uri: `${url}${videoId}`,
+                timeout: 5000
+            });
+
+            const regexMatches = /(?:var frame_url = ")(.*)(?:")/g.exec(videoPageHtml);
+
+            if (regexMatches.length === 2) {
+                let videoStreamUrl = `https:${regexMatches[1]}`;
+
+                const page = await browser.newPage();
+                await page.goto(videoStreamUrl);
+                await page.waitFor(() => !!document.querySelector('video').src);
+                const videoSrc = await page.$eval('video', video => video.src);
+                await page.close();
+                const videoSourceUrl = URL.parse(videoSrc, true).query.url;
+
+                sse.send({videoSourceUrl, provider: url}, 'results');
+            }
+
+
+        } catch (err) {
+            console.log(err);
+            if (err.cause && err.cause.code !== 'ETIMEDOUT') {
+                console.error(err);
+                sse.send({url, message: 'Looks like this provider is down.'}, 'error');
+            }
+        }
+    }
+
+    // Asyncronously start all the scrapers for each url
+    urls.forEach((url) => {
+        promises.push(scrape(url));
+    })
+
+    // Wait for all the scrapers to return before closing the browser
+    await Promise.all(promises);
+    await browser.close();
+}
+
+module.exports = exports = Afdah;
--- providers/movies/Afdah.js
+++ providers/movies/Afdah.js
@@ -3,21 +3,25 @@ const Promise = require('bluebird');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
+const tough = require('tough-cookie');
+const randomUseragent = require('random-useragent');
 
-async function Afdah(queryString, sse) {
+async function Afdah(req, sse) {
+    const movieTitle = req.query.title;
+
     // Start up the headless browser in no-sandbox mode to make it truly headless
     const browser = await puppeteer.launch({args: ['--no-sandbox']});
-    
+
     // These are all the same host I think. https://xmovies8.org isn't loading.
     const urls = ["https://afdah.org", "https://genvideos.com", "https://genvideos.co", "https://watch32hd.co", "https://putlockerhd.co", "https://xmovies8.org"];
     const promises = [];
 
-    // Go to each url and scrape for links, then send the link to the client 
+    // Go to each url and scrape for links, then send the link to the client
     async function scrape(url) {
         try {
-            const usePlus = url === "https://putlockerhd.co" || url === "https://genvideos.co" || url === "https://watch32hd.co"
+            const usePlus = url === "https://putlockerhd.co" || url === "https://genvideos.co";
             const html = await rp({
-                uri: `${url}/results?q=${usePlus ? queryString.replace(/ /, '%2B'): queryString}`,
+                uri: `${url}/results?q=${usePlus ? movieTitle.replace(/ /g, '+'): movieTitle.replace(/ /g, '%20')}`,
                 timeout: 5000
             });
 
@@ -26,7 +30,7 @@ async function Afdah(queryString, sse) {
 
             $('.cell').toArray().some(element => {
                 const videoName = $(element).find('.video_title').text().trim();
-                if (videoName === queryString) {
+                if (videoName === movieTitle) {
                     videoId = $(element).find('.video_title h3 a').attr('href').trim();
                     return true;
                 }
@@ -44,17 +48,81 @@ async function Afdah(queryString, sse) {
             if (regexMatches.length === 2) {
                 let videoStreamUrl = `https:${regexMatches[1]}`;
 
-                const page = await browser.newPage();
-                await page.goto(videoStreamUrl);
-                await page.waitFor(() => !!document.querySelector('video').src);
-                const videoSrc = await page.$eval('video', video => video.src);
-                await page.close();
-                const videoSourceUrl = URL.parse(videoSrc, true).query.url;
-                
+                var jar = rp.jar();
+                const videoPageHtml = await rp({
+                    uri: videoStreamUrl,
+                    jar,
+                    timeout: 5000
+                });
+
+                // const userAgent = randomUseragent.getRandom();
+                const userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36 OPR/55.0.2994.44';
+
+                const postID = /(?:var postID = ')(.*)(?:';)/.exec(videoPageHtml)[1];
+                const viewData = await rp({
+                    uri: 'https://vidlink.org/embed/update_views',
+                    method: 'POST',
+                    formData: {},
+                    headers: {
+                        accept: 'application/json, text/javascript, */*; q=0.01',
+                        'content-length': 0,
+                        'accept-language': 'en-US,en;q=0.9',
+                        // 'accept-encoding': 'gzip, deflate, br',
+                        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                        dnt: 1,
+                        origin: 'https://vidlink.org',
+                        referer: videoStreamUrl,
+                        'save-data': 'on',
+                        'user-agent': userAgent,
+                        'x-requested-with': 'XMLHttpRequest'
+                    },
+                    jar,
+                    gzip: true,
+                    json: true,
+                    timeout: 5000
+                });
+                const id_view = viewData.id_view;
+
+                const obfuscatedSources = await rp({
+                    uri: 'https://vidlink.org/streamdrive/info',
+                    method: 'POST',
+                    headers: {
+                        accept: 'text/html, */*; q=0.01',
+                        'accept-language': 'en-US,en;q=0.9',
+                        // 'accept-encoding': 'gzip, deflate, br',
+                        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                        dnt: 1,
+                        origin: 'https://vidlink.org',
+                        referer: videoStreamUrl,
+                        'save-data': 'on',
+                        'user-agent': userAgent,
+                        'x-requested-with': 'XMLHttpRequest'
+                    },
+                    formData: {
+                        browserName: 'Opera',
+                        platform: 'Linux x86_64',
+                        postID,
+                        id_view
+                    },
+                    jar,
+                    timeout: 5000
+                });
+
+                const cleanedObfuscatedSources = obfuscatedSources
+                    .replace('return(c35?String', `return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String`);
+
+
+                const vm = require('vm');
+                const sandbox = {window: {checkSrc: function(){}}}; // starting variables
+                vm.createContext(sandbox); // Contextify the sandbox.
+                vm.runInContext(cleanedObfuscatedSources, sandbox);
+                videoSourceUrl = sandbox.window.srcs[0].url;
+                videoSourceSize = sandbox.window.srcs[0].size;
+
                 sse.send({videoSourceUrl, provider: url}, 'results');
             }
 
-            
+
         } catch (err) {
             console.log(err);
             if (err.cause && err.cause.code !== 'ETIMEDOUT') {
@@ -68,7 +136,7 @@ async function Afdah(queryString, sse) {
     urls.forEach((url) => {
         promises.push(scrape(url));
     })
-    
+
     // Wait for all the scrapers to return before closing the browser
     await Promise.all(promises);
     await browser.close();
