5b7cda4e6e7c20a536f77eb5d0c365b1ddc166c1
ddivad <david_d195@protonmail.com>
2018-10-02 23:40:26 +0100

    initial structure for GoWatchSeries. Working but need to get the video url

---
+++ providers/tv/GoWatchSeries.js
@@ -0,0 +1,103 @@
+const URL = require('url');
+const Promise = require('bluebird');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+const logger = require('../../utils/logger')
+
+async function GoWatchSeries(req, sse) {
+    const showTitle = req.query.title;
+    const { season, episode } = req.query;
+    console.log(season)
+
+    const urls = ['https://gowatchseries.co'];
+    const promises = [];
+    var jar = rp.jar();
+
+    async function scrape(url) {
+        try {
+            const html = await rp({
+                uri: `${url}/search.html?keyword=${showTitle.replace(/ /, '%20')}`,
+                headers: {
+                    // 'user-agent': userAgent,
+                    // 'x-real-ip': req.client.remoteAddress,
+                    // 'x-forwarded-for': req.client.remoteAddress
+                },
+                jar,
+                timeout: 5000
+            });
+
+            let $ = cheerio.load(html);
+
+            const seasonLinks = []
+            const seasonLink = $('.hover_watch').toArray().find((moviePoster) => {
+                const link = $(moviePoster.parent).attr('href');
+                return link.includes(`${showTitle}-season-${season}`)
+            });
+            const seasonPageLink = `${url}/${$(seasonLink.parent).attr('href')}`
+            console.log(seasonPageLink)
+           
+            const seasonPageHtml = await rp({
+                uri: `${seasonPageLink}`,
+                headers: {
+                    // 'user-agent': userAgent,
+                    // 'x-real-ip': req.client.remoteAddress,
+                    // 'x-forwarded-for': req.client.remoteAddress
+                },
+                jar,
+                timeout: 5000
+            });
+
+            $ = cheerio.load(seasonPageHtml);
+            const episodePageLink = $('.child_episode').toArray().find((e) => {
+                const link = $(e).find('a').attr('href');
+                return link.includes(`episode-${episode}`)
+            })
+            const episodeLink = `${url}${$(episodePageLink).find('a').attr('href')}`;
+
+            const episodePageHtml = await rp({
+                uri: `${episodeLink}`,
+                headers: {
+                    // 'user-agent': userAgent,
+                    // 'x-real-ip': req.client.remoteAddress,
+                    // 'x-forwarded-for': req.client.remoteAddress
+                },
+                jar,
+                timeout: 5000
+            });
+            
+            $ = cheerio.load(episodePageHtml);
+            const videoDiv = $('.play-video');
+            videoDiv.children().toArray().forEach((child) => {
+                if(child.name === 'iframe') {
+                    // console.log(child.attribs.src)
+                    console.log(child)
+                    
+                }
+            })
+            // console.log($('.jw-video'))
+
+        } catch (e) {
+            console.log(e)
+        }
+
+    }
+    urls.forEach((url) => {
+        promises.push(scrape(url));
+    })
+
+    await Promise.all(promises);
+
+}
+
+const test = {
+    query: {
+        season: 6,
+        episode: 8,
+        title: 'suits'
+    }
+}
+
+GoWatchSeries(test, {})
+
+// module.exports = exports = GoWatchSeries;
