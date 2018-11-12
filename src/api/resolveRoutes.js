ca0a21b0b584a178c782ca7958e4aac5649918df
NBTX <nbtx@telegenic.me>
2018-11-12 17:49:52 +0000

    Change source of HTML from `req.body.html` to `req.body` - no point having separate POST parameters.

--- src/api/resolveRoutes.js
+++ src/api/resolveRoutes.js
@@ -25,7 +25,7 @@ resolveRoutes.post('/:resolver', verifyToken, async (req, res) => {
     // need to parse cookie possibly
     const jar = rp.jar();
     try {
-        const data = await resolveHtml(Buffer.from(req.body.html, 'base64').toString(), req.params.resolver, jar, req.body.headers)
+        const data = await resolveHtml(Buffer.from(req.body, 'base64').toString(), req.params.resolver, jar, req.body.headers)
         res.json({data});
     } catch(err) {
         res.status(500).send();
